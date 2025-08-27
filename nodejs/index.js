e25bfbeaa6803a17fee31cd48e044fbad9c588ac
Kenneth Foo <101826160+kennethfoo24@users.noreply.github.com>
2025-08-27 16:37:46 +0800

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const axios = require('axios');
+const { createLogger, format, transports } = require('winston');
+const path = require('path');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.json()
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: path.join(__dirname, 'application.log') }) 
+  ]
+});
 
 dotenv.config();
 
@@ -12,6 +26,9 @@ const PYTHON_SERVICE_URL = process.env.PYTHON_SERVICE_URL;
 // Respond "Hello world!" on /
 app.get('/nodejs', async (req, res) => {
   console.log("This is a nodeJS log");
+  logger.info('This is just an info log written in JSON to test trace-log correlation')
+  logger.error('This is an ERROR log written in JSON. The house is on fire!', {fruit: 'apple'});
+  logger.log('error', 'This is an ERROR log written in JSON. error: Internal Server Error',{fruit: 'orange' });
   try {
     // Call the other service by its k8s Service DNS name
     const otherRes = await axios.get(PYTHON_SERVICE_URL, {
